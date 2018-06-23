865a70936ebd5d25fc85524b087f419f5575eb26
Ju <jluuiss@gmail.com>
2018-06-24 03:09:41 +0800

    Fix tabspaces

--- app.py
+++ app.py
@@ -10,29 +10,29 @@ parser = reqparse.RequestParser()
 
 class Main(Resource):
   def get(self):
-      return {'hello': 'world'}
+    return {'hello': 'world'}
 
   def post(self):
-      args = parser.parse_args()
-      html = args["html"]
-      text = get_article_from_html(html).text
-      quiz = generate_quiz(text)
-      #links = getrelatedlinks(getKeywords(text), 5)
+    args = parser.parse_args()
+    html = args["html"]
+    text = get_article_from_html(html).text
+    quiz = generate_quiz(text)
+    #links = getrelatedlinks(getKeywords(text), 5)
 
 def generate_quiz(text):
-    headers = {'content-type': 'text/plain'}
-    quiz_json = requests.post('https://gs4ossx7yj.execute-api.us-east-1.amazonaws.com/dev/text', headers = headers, data = json.dumps({"data": "\"" + text + "\""}))
-    quiz_json_text = json.loads(quiz_json.text)[0]
-    return json.dumps({"question": quiz_json_text[u'QuestionPrompt'], "type": quiz_json_text[u'QuestionType'], "answer": quiz_json_text[u'CorrectAnswer']})
+  headers = {'content-type': 'text/plain'}
+  quiz_json = requests.post('https://gs4ossx7yj.execute-api.us-east-1.amazonaws.com/dev/text', headers = headers, data = json.dumps({"data": "\"" + text + "\""}))
+  quiz_json_text = json.loads(quiz_json.text)[0]
+  return json.dumps({"question": quiz_json_text[u'QuestionPrompt'], "type": quiz_json_text[u'QuestionType'], "answer": quiz_json_text[u'CorrectAnswer']})
 
 # Call the function by passing in an array of keywords
 # and desired number of links, output is an array of related links
 def getrelatedlinks(keywords, numoflinks):
-    searchkey = ' '.join(keywords)
-    links = []
-    for url in search(searchkey, stop=numoflinks):
-        links.append(url)
-    return links
+  searchkey = ' '.join(keywords)
+  links = []
+  for url in search(searchkey, stop=numoflinks):
+    links.append(url)
+  return links
 
 api.add_resource(Main, '/')
 
--- utils/text_cleaner.py
+++ utils/text_cleaner.py
@@ -3,21 +3,21 @@ from newspaper import Article
 from bs4 import BeautifulSoup
 
 def get_article_from_url(article_url) :
-    # Returns a `newspaper` Article object from article URL
-    article = Article(article_url, keep_article_html=True)
-    article.download()
-    article.parse()
-    attach_links(article)
-    return article
+  # Returns a `newspaper` Article object from article URL
+  article = Article(article_url, keep_article_html=True)
+  article.download()
+  article.parse()
+  attach_links(article)
+  return article
 
 def get_article_from_html(article_html) :
-    # Returns a `newspaper` Article object from article HTML
-    article = Article('', keep_article_html=True)
-    article.set_html(article_html)
-    article.parse()
-    attach_links(article)
-    return article
+  # Returns a `newspaper` Article object from article HTML
+  article = Article('', keep_article_html=True)
+  article.set_html(article_html)
+  article.parse()
+  attach_links(article)
+  return article
 
 def attach_links(article) :
-    # Attaches array of links embedded in article
-    article.links = list(map(lambda a: a.get('href'), BeautifulSoup(article.article_html, 'html.parser').find_all('a')))
+  # Attaches array of links embedded in article
+  article.links = list(map(lambda a: a.get('href'), BeautifulSoup(article.article_html, 'html.parser').find_all('a')))
