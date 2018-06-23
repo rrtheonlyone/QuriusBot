44fed17220a13aa479b7ee65a2c0803b75875d0f
Ju <jluuiss@gmail.com>
2018-06-24 00:17:04 +0800

    Use newspapers library

--- README.md
+++ README.md
@@ -1,6 +1,5 @@
 #QuriusBot Server
 
 # Setup: #
-1. Sign up for a key at https://mercury.postlight.com/web-parser/
-2. Make a `utils/secrets.py` file. Copy the key as `mercury_api_key` into this file.
-3. Run `app.py` i guess lol
+1. Install dependencies
+2. Run `app.py` i guess lol
--- utils/text_cleaner.py
+++ utils/text_cleaner.py
@@ -1,25 +1,23 @@
 import requests as Requests
-import secrets
+from newspaper import Article
 from bs4 import BeautifulSoup
 
-def get_article(article_url) :
-    # Returns the main content of the article as an object of html strings
-    headers = {'x-api-key':secrets.mercury_api_key, 'Content-Type':'application/json'}
-    response = Requests.get('https://mercury.postlight.com/parser?url='+article_url, headers=headers)
-    return response.json()
+def get_article_from_url(article_url) :
+    # Returns a `newspaper` Article object from article URL
+    article = Article(article_url, keep_article_html=True)
+    article.download()
+    article.parse()
+    attach_links(article)
+    return article
 
-def get_clean_text(html_text) :
-    # Returns cleaned text from html string
-    return BeautifulSoup(html_text, 'html.parser').get_text()
+def get_article_from_html(article_html) :
+    # Returns a `newspaper` Article object from article HTML
+    article = Article('', keep_article_html=True)
+    article.set_html(article_html)
+    article.parse()
+    attach_links(article)
+    return article
 
-def get_embedded_links(html_text) :
-    # Returns array of links embedded in article
-    return map(lambda a: a.get('href'), BeautifulSoup(html_text, 'html.parser').find_all('a'))
-
-def clean_article(uncleaned_article) :
-    # Returns cleaned article
-    return {
-        **uncleaned_article,
-        'content': get_clean_text(uncleaned_article['content']),
-        'embedded_links': get_embedded_links(uncleaned_article['content']),
-    }
+def attach_links(article) :
+    # Attaches array of links embedded in article
+    article.links = list(map(lambda a: a.get('href'), BeautifulSoup(article.article_html, 'html.parser').find_all('a')))
