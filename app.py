9abc79a5aae8f8a2ea374f123fc36cf8b2ec7814
Rahul Rajesh <rahul.rajesh@hotmail.com>
2018-06-23 23:39:47 +0800

    Moved word extraction out

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ api = Api(app)
 parser = reqparse.RequestParser()
 
 class Main(Resource):
-    
+
   def get(self):
       return {'hello': 'world'}
   
@@ -21,71 +21,6 @@ class Main(Resource):
       quiz = generate_quiz(text)
       #links = getrelatedlinks(getKeywords(text), 5)
 
-def isPunct(word):
-    return len(word) == 1 and word in string.punctuation
-
-def isNumeric(word):
-    try:
-      float(word) if '.' in word else int(word)
-      return True
-    except ValueError:
-      return False
-
-def _generate_candidate_keywords(sentences):
-  stopwords = set(nltk.corpus.stopwords.words())
-  phrase_list = []
-  for sentence in sentences:
-    words = map(lambda x: "|" if x in stopwords else x,
-      nltk.word_tokenize(sentence.lower()))
-    phrase = []
-    for word in words:
-      if word == "|" or isPunct(word):
-        if len(phrase) > 0:
-          phrase_list.append(phrase)
-          phrase = []
-      else:
-        phrase.append(word)
-  return phrase_list
-
-def _calculate_word_scores(phrase_list):
-  word_freq = nltk.FreqDist()
-  word_degree = nltk.FreqDist()
-  for phrase in phrase_list:
-    degree = len(filter(lambda x: not isNumeric(x), phrase)) - 1
-    for word in phrase:
-      word_freq.inc(word)
-      word_degree.inc(word, degree) # other words
-  for word in word_freq.keys():
-    word_degree[word] = word_degree[word] + word_freq[word] # itself
-  # word score = deg(w) / freq(w)
-  word_scores = {}
-  for word in word_freq.keys():
-    word_scores[word] = word_degree[word] / word_freq[word]
-  return word_scores
-
-def _calculate_phrase_scores(phrase_list, word_scores):
-  phrase_scores = {}
-  for phrase in phrase_list:
-    phrase_score = 0
-    for word in phrase:
-      phrase_score += word_scores[word]
-    phrase_scores[" ".join(phrase)] = phrase_score
-  return phrase_scores
-  
-def extract(text, incl_scores=False):
-  sentences = nltk.sent_tokenize(text)
-  phrase_list = _generate_candidate_keywords(sentences)
-  word_scores = _calculate_word_scores(phrase_list)
-  phrase_scores = _calculate_phrase_scores(
-    phrase_list, word_scores)
-  sorted_phrase_scores = sorted(phrase_scores.iteritems(),
-    key=operator.itemgetter(1), reverse=True)
-  n_phrases = len(sorted_phrase_scores)
-  if incl_scores:
-    return sorted_phrase_scores[0:int(n_phrases)]
-  else:
-    return map(lambda x: x[0],
-      sorted_phrase_scores[0:int(n_phrases)])
 
 def get_clean_text(html_text) :
 # Returns cleaned text from html string
