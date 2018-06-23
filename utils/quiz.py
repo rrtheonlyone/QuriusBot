c60d76e90727c8629ba40cf121de8179688c1a0e
Sreyans Sipani <sreyans.sipani99@gmail.com>
2018-06-24 04:54:44 +0800

    Fix bug in quiz generator

--- utils/quiz.py
+++ utils/quiz.py
@@ -4,7 +4,7 @@ import simplejson as json
 def generate_quiz(text):
     headers = {'content-type': 'text/plain'}
     quiz_json = requests.post('https://gs4ossx7yj.execute-api.us-east-1.amazonaws.com/dev/text', headers = headers, data = json.dumps({"data": "\"" + text + "\""}))
-    quiz_json_text = json.loads(quiz_json.text)[0]
-    return json.dumps({"question": quiz_json_text[u'QuestionPrompt'], "type": quiz_json_text[u'QuestionType'], "answer": quiz_json_text[u'CorrectAnswer']})    
-
-
+    quiz_json_text = json.loads(quiz_json.text)
+    for index in range(len(quiz_json_text)):
+        quiz_json_text[index] = json.dumps({"question": quiz_json_text[index][u'QuestionPrompt'], "type": quiz_json_text[index][u'QuestionType'], "answer": quiz_json_text[index][u'CorrectAnswer']})
+    return quiz_json_text
