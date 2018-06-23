b81fb6c0c685a06ae916150d62600024288132a1
Ju <jluuiss@gmail.com>
2018-06-24 02:54:54 +0800

    Fix keyword extractor

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
+from __future__ import division
 from flask import Flask
 from flask_restful import reqparse, Resource, Api
+from utils.text_cleaner import get_article_from_html, get_article_from_url
+from utils.keywords_extractor import extract_keywords
 
 app = Flask(__name__)
 api = Api(app)
@@ -8,24 +11,19 @@ parser = reqparse.RequestParser()
 class Main(Resource):
   def get(self):
       return {'hello': 'world'}
-  
+
   def post(self):
       args = parser.parse_args()
       html = args["html"]
-      text = get_clean_text(html)
+      text = get_article_from_html(html).text
       quiz = generate_quiz(text)
       #links = getrelatedlinks(getKeywords(text), 5)
-
-
-def get_clean_text(html_text) :
-	# Returns cleaned text from html string
-    return BeautifulSoup(html_text, 'html.parser').get_text()
 
 def generate_quiz(text):
     headers = {'content-type': 'text/plain'}
     quiz_json = requests.post('https://gs4ossx7yj.execute-api.us-east-1.amazonaws.com/dev/text', headers = headers, data = json.dumps({"data": "\"" + text + "\""}))
     quiz_json_text = json.loads(quiz_json.text)[0]
-    return json.dumps({"question": quiz_json_text[u'QuestionPrompt'], "type": quiz_json_text[u'QuestionType'], "answer": quiz_json_text[u'CorrectAnswer']})    
+    return json.dumps({"question": quiz_json_text[u'QuestionPrompt'], "type": quiz_json_text[u'QuestionType'], "answer": quiz_json_text[u'CorrectAnswer']})
 
 # Call the function by passing in an array of keywords
 # and desired number of links, output is an array of related links
@@ -40,5 +38,3 @@ api.add_resource(Main, '/')
 
 if __name__ == '__main__':
 	app.run(debug=True)
-
-
---
+++ utils/keywords_extractor.py
@@ -0,0 +1,30 @@
+from collections import Counter
+import math
+import nltk
+from nltk.corpus import stopwords
+from nltk.stem import WordNetLemmatizer
+from nltk.tokenize import RegexpTokenizer
+
+def extract_keywords(text):
+    # First segment into sentences
+    sentences = nltk.sent_tokenize(text)
+    token_frequencies = [None] * len(sentences)
+    idf = {}
+    tokens = set()
+
+    for index, sentence in enumerate(sentences):
+        tokenizer = RegexpTokenizer(r'\w+')
+        words = tokenizer.tokenize(sentence)
+        lemmatizer = WordNetLemmatizer()
+        words = [lemmatizer.lemmatize(word) for word in words]
+        stop_words = stopwords.words('english')
+        words = [word for word in words if word not in stop_words]
+        for word in words:
+            tokens.add(word)
+        token_frequencies[index] = Counter(words)
+
+    for token in tokens:
+        idf[token] = math.log(len(sentences) / len([index for index, sentence in enumerate(sentences) if token_frequencies[index][token] > 0]))
+
+    ranked_words = sorted(list(tokens), key=idf.__getitem__)[:5]
+    return ranked_words
