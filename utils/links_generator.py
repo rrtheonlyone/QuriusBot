132e98193b26a2c59869df648bd71b98f9ccd357
Ju <jluuiss@gmail.com>
2018-06-24 04:35:17 +0800

    Quiz API

--- utils/links_generator.py
+++ utils/links_generator.py
@@ -1,12 +1,14 @@
 from collections import Counter
+import requests
 import math
 import nltk
+import utils.secrets as secrets
 from nltk.corpus import stopwords
 from nltk.stem import WordNetLemmatizer
 from nltk.tokenize import RegexpTokenizer
 
 def extract_keywords(text):
-  # First segment into sentences
+  # Returns array of 5 top words
   sentences = nltk.sent_tokenize(text)
   token_frequencies = [None] * len(sentences)
   idf = {}
@@ -28,3 +30,13 @@ def extract_keywords(text):
 
   ranked_words = sorted(list(tokens), key=idf.__getitem__)[:5]
   return ranked_words
+
+def get_links_with_words(keywords):
+  # Returns array of URLs
+  headers={"Ocp-Apim-Subscription-Key":secrets.ocp_key}
+  search_query = " ".join(keywords)
+  response = requests.get('https://api.cognitive.microsoft.com/bing/v7.0/search?q='+search_query+'&count=5',headers=headers)
+  return [result['url'] for result in response.json()['webPages']['value']]
+
+def get_links_for_article(article):
+  return get_links_with_words(extract_keywords(article))
